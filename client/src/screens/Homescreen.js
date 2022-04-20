6307c1aeb2a6d4b393f025dd966074d04cfb0895
djolodjolo <tkd_djole@rocketmail.com>
2022-04-20 03:03:52 +0200

    Added timeline and cube-skills animation

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,6 +1,15 @@
-import React,{useEffect} from 'react';
+import React, {useEffect} from 'react';
 import './Home.css';
-
+import * as THREE from 'three'; //javascript 3d library
+import {Typography} from '@mui/material';
+import TimeLine from '../components/TimeLine';
+import html from '../images/html.png';
+import css from '../images/css.png';
+import js from '../images/javascript.png'
+import php from '../images/php.png';
+import mongo from '../images/mongodb.png';
+import c_sharp from '../images/c_sharp.png';
+//import {TimeLine} from '../components/TimeLine';
 
 
 
@@ -8,12 +17,52 @@ const Homescreen = () => {
 
     useEffect(() => {
      
+      
+
+      
     }, [])
     
 
   return (
     <div className='home'>
-        <canvas className='homeCanvas'></canvas>
+        <canvas className='homeCanvas'> </canvas>
+        <div className='homeContainer'>
+           <Typography variant='h3'>TIMELINE</Typography>
+           <TimeLine timelines={[1,2,3,4]}></TimeLine>
+        </div>
+        <div className='homeSkills'>
+          <Typography variant='h3'>Skills</Typography>
+          <div className='homeCubeSkills'>
+            <div className='homeCubeSkillsFaces homeCubeSkillsFace1'>
+            <img src={html} width="100px" height="100px" alt='Face1' />
+
+            </div>
+            <div className='homeCubeSkillsFaces homeCubeSkillsFace2'>
+            <img src={css} width="100px" height="100px" alt='Face2' />
+
+            </div>
+            <div className='homeCubeSkillsFaces homeCubeSkillsFace3'>
+            <img src={js} width="100px" height="100px" alt='Face3' />
+
+            </div>
+            <div className='homeCubeSkillsFaces homeCubeSkillsFace4'>
+            <img src={php} width="100px" height="100px" alt='Face4' />
+
+            </div>
+            <div className='homeCubeSkillsFaces homeCubeSkillsFace5'>
+            <img src={mongo} width="100px" height="100px" alt='Face5' />
+
+            </div>
+            <div className='homeCubeSkillsFaces homeCubeSkillsFace6'>
+            <img src={c_sharp} width="100px" height="100px" alt='Face6' />
+
+            </div>
+
+          </div>
+          <div className='cubeShadow'></div>
+        </div>
+        
+        
     </div>
   )
 }
